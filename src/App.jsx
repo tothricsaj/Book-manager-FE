1896f021ee4122c0b0bb6556de6cdeeb446cb722
tothricsaj <tothricsaj@gmail.com>
2022-07-02 10:58:36 +0200

    feat: added book list component

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import BookList from './components/Book/book-list/book-list';
+
+export function App() {
+  return (
+    <>
+      <BookList />
+    </>
+  );
+}
---
+++ src/components/Book/book-list/book-list.jsx
@@ -0,0 +1,5 @@
+export default BookList = () => {
+	return (
+		<h2>BookList</h2>
+	);
+}
