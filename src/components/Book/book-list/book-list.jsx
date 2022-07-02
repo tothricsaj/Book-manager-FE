56295c58faa5f94361b3f079b1792c17e28cc53c
tothricsaj <tothricsaj@gmail.com>
2022-07-02 11:55:35 +0200

    feat: basic fetch

--- src/components/Book/book-list/book-list.jsx
+++ src/components/Book/book-list/book-list.jsx
@@ -1,6 +1,55 @@
+import React, { useState, useEffect, useMemo } from 'react';
+
+const gqlQuery = {
+	query: `
+		query BookList {
+			books {
+				id
+				title
+				author
+				genre
+				pubYear
+			}
+		}
+	`
+}
+
 export const BookList = () => {
+
+	const [bookList, setBookList] = useState([]);
+
+	useEffect(() => {
+		const fetchingData = async () => {
+			const response = await fetch('http://localhost:3005/graphql', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(gqlQuery)
+			});
+			const fetchedBooks = await response.json();
+
+			setBookList([...fetchedBooks.data.books]);
+		}
+
+		fetchingData();
+
+		console.log(bookList);
+	}, []);
+
+	const books = useMemo(() => {
+		return bookList.map(book => (
+			<li key={book.id}>
+				{book.title}
+			</li>
+		))
+	}, [bookList]);
+
 	return (
-		<h2>BookList</h2>
+		<>
+			<h2>BookList</h2>
+			<ul>{ books }</ul>
+		</>
 	);
 }
 
