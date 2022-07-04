36b4a4e6092d714585ed32c522bf571543a72914
tothricsaj <tothricsaj@gmail.com>
2022-07-04 08:49:18 +0200

    feat: added basic style

--- src/components/Book/book-list-element/book-list-element.jsx
+++ src/components/Book/book-list-element/book-list-element.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import * as classes from './book-list-element.module.css';
 
 export const BookListElementFragment = `
 		fragment BookListElementFragment on Book {
@@ -14,7 +15,7 @@ export const BookListElement = ({ data }) => {
 	const {id, title, author, genre, pubYear} = data;
 
 	return (
-		<li data-book-id={id}>
+		<li data-book-id={id} className={classes.wrapper}>
 			<h3>{ title }</h3>
 			<h4>{ author }</h4>
 			<p>{ genre }</p>
