60f5a8252c2833a1c66299d749375362fb1de617
tothricsaj <tothricsaj@gmail.com>
2022-07-05 17:33:46 +0200

    feat: added book page query

--- src/components/Book/book-page/book-page.jsx
+++ src/components/Book/book-page/book-page.jsx
@@ -1,13 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
+
+const graphqlQuery = {
+	query: 
+		`
+			query BookById($bookById: queryParamInput) {
+				complexBookSearch(params: $bookById) {
+					title
+					author
+					genre
+				}
+			}
+		`,
+}
 
 export const BookPage = () => {
 	const urlParams = useParams();
+	const [book, setBook] = useState({});
+
 	useEffect(() => {
-		console.log('BookPage');
+
+		graphqlQuery.variables = {
+			bookById: {
+				id: +urlParams.bookId
+			}
+		};
+
+		const fetchingData = async () => {
+			const response = await fetch('http://localhost:3005/graphql', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(graphqlQuery)
+			});
+			const fetchedBook = await response.json();
+
+			setBook({...fetchedBook.data.complexBookSearch[0]});
+		}
+
+		fetchingData();
 	}, []);
 
 	return (
-		<h2>Book Page {urlParams.bookId}</h2>
+		<>
+			<h2>{book.title}</h2>
+		</>
 	)
 };
